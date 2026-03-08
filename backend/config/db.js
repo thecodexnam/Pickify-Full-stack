e261f71ac80959ed795892fa223de5f866cf700a
namanydv <namanofficial91@gmail.com>
2026-03-08 23:29:03 +0530

    feat: add MongoDB connection setup using Mongoose and dotenv.

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URL)
-        console.log(`DataBase Connected Successfully`);
+        console.log(`DataBase is Connected Successfully`);
         
     } catch (error) {
         console.error("Error connecting to MongoDB:", error)
