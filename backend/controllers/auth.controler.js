faf0aa99aa7e928b9951c40331fd8907293aaaea
namanydv <namanofficial91@gmail.com>
2026-03-09 23:29:34 +0530

    .

--- backend/controllers/auth.controler.js
+++ backend/controllers/auth.controler.js
@@ -23,8 +23,8 @@ const signUp = async (req,res)=>{
                 mobile,
                 role
             })
-            await newUser.save()
-            res.status(201).json({message:"User created successfully"})
+            // await newUser.save()
+            // res.status(201).json({message:"User created successfully"})
         
     } catch (error) {
         
--- backend/utils/token.js
+++ backend/utils/token.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 const genToken = async (userID) =>{
     try {
-        const token = await jwt.sign()
+        const token = await jwt.sign({userID},process.env.JWT_SECRET,{expireIn:"7d"})
         
     } catch (error) {
         
